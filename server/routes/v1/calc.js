c8da9a5b9f5eb097b48a496f3c6c21493228c18f
phamhoangviet261 <hoangviet.it.hcmus@gmail.com>
2023-05-17 10:39:41 +0700

    fix: fix month add new inv

--- server/routes/v1/calc.js
+++ server/routes/v1/calc.js
@@ -29,7 +29,7 @@ router.post('/', async (req, res, next) => {
         const invoices = await Invoice.find({});
         var today = new Date();
         var day = String(today.getDate()).padStart(2, '0');
-        var month = String(today.getMonth() + 2).padStart(2, '0'); //January is 0!
+        var month = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
         var year = today.getFullYear();
 
         let r = []

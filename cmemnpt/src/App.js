71460a8e32733ebcf54b98e36d753229d6d6ea20
kuokkutin <tranquocthinh.t2.15cla@gmail.com>
2023-05-19 16:05:43 +0700

    Change some UI

--- cmemnpt/src/App.js
+++ cmemnpt/src/App.js
@@ -3,14 +3,14 @@ import axios from 'axios'
 import { useMutation, useQuery, useQueryClient } from 'react-query'
 
 
-function App() {  
+function App() {
   const SERVER = 'https://api-mnpt-cme.vercel.app/api/v1'
   // const SERVER = 'http://localhost:5001/api/v1'
   // const [invoices, setInvoices] = useState();
   // const [accounts, setAccounts] = useState();
   // const [totalInMonth, setTotalInMonth] = useState([]);
   const [pw, setPw] = useState();
-  useEffect(() =>{
+  useEffect(() => {
     const callAPI = async () => {
       // const month = new Date().getMonth() + 2;
       // const year = new Date().getFullYear();
@@ -42,160 +42,154 @@ function App() {
     keepPreviousData: true,
     retry: 0
   })
-  console.log({getInvoices})
+  console.log({ getInvoices })
   const clearAll = async () => {
-    if(pw != 10052008) return;
+    if (pw != 10052008) return;
     var nVer = navigator.appVersion;
     var nAgt = navigator.userAgent;
-    var browserName  = navigator.appName;
-    var fullVersion  = ''+parseFloat(navigator.appVersion); 
-    var majorVersion = parseInt(navigator.appVersion,10);
-    var nameOffset,verOffset,ix;
+    var browserName = navigator.appName;
+    var fullVersion = '' + parseFloat(navigator.appVersion);
+    var majorVersion = parseInt(navigator.appVersion, 10);
+    var nameOffset, verOffset, ix;
 
     // In Opera, the true version is after "Opera" or after "Version"
-    if ((verOffset=nAgt.indexOf("Opera"))!=-1) {
-    browserName = "Opera";
-    fullVersion = nAgt.substring(verOffset+6);
-    if ((verOffset=nAgt.indexOf("Version"))!=-1) 
-      fullVersion = nAgt.substring(verOffset+8);
+    if ((verOffset = nAgt.indexOf("Opera")) != -1) {
+      browserName = "Opera";
+      fullVersion = nAgt.substring(verOffset + 6);
+      if ((verOffset = nAgt.indexOf("Version")) != -1)
+        fullVersion = nAgt.substring(verOffset + 8);
     }
     // In MSIE, the true version is after "MSIE" in userAgent
-    else if ((verOffset=nAgt.indexOf("MSIE"))!=-1) {
-    browserName = "Microsoft Internet Explorer";
-    fullVersion = nAgt.substring(verOffset+5);
+    else if ((verOffset = nAgt.indexOf("MSIE")) != -1) {
+      browserName = "Microsoft Internet Explorer";
+      fullVersion = nAgt.substring(verOffset + 5);
     }
     // In Chrome, the true version is after "Chrome" 
-    else if ((verOffset=nAgt.indexOf("Chrome"))!=-1) {
-    browserName = "Chrome";
-    fullVersion = nAgt.substring(verOffset+7);
+    else if ((verOffset = nAgt.indexOf("Chrome")) != -1) {
+      browserName = "Chrome";
+      fullVersion = nAgt.substring(verOffset + 7);
     }
     // In Safari, the true version is after "Safari" or after "Version" 
-    else if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
-    browserName = "Safari";
-    fullVersion = nAgt.substring(verOffset+7);
-    if ((verOffset=nAgt.indexOf("Version"))!=-1) 
-      fullVersion = nAgt.substring(verOffset+8);
+    else if ((verOffset = nAgt.indexOf("Safari")) != -1) {
+      browserName = "Safari";
+      fullVersion = nAgt.substring(verOffset + 7);
+      if ((verOffset = nAgt.indexOf("Version")) != -1)
+        fullVersion = nAgt.substring(verOffset + 8);
     }
     // In Firefox, the true version is after "Firefox" 
-    else if ((verOffset=nAgt.indexOf("Firefox"))!=-1) {
-    browserName = "Firefox";
-    fullVersion = nAgt.substring(verOffset+8);
+    else if ((verOffset = nAgt.indexOf("Firefox")) != -1) {
+      browserName = "Firefox";
+      fullVersion = nAgt.substring(verOffset + 8);
     }
     // In most other browsers, "name/version" is at the end of userAgent 
-    else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < 
-              (verOffset=nAgt.lastIndexOf('/')) ) 
-    {
-    browserName = nAgt.substring(nameOffset,verOffset);
-    fullVersion = nAgt.substring(verOffset+1);
-    if (browserName.toLowerCase()==browserName.toUpperCase()) {
-      browserName = navigator.appName;
-    }
+    else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) <
+      (verOffset = nAgt.lastIndexOf('/'))) {
+      browserName = nAgt.substring(nameOffset, verOffset);
+      fullVersion = nAgt.substring(verOffset + 1);
+      if (browserName.toLowerCase() == browserName.toUpperCase()) {
+        browserName = navigator.appName;
+      }
     }
     // trim the fullVersion string at semicolon/space if present
-    if ((ix=fullVersion.indexOf(";"))!=-1)
-      fullVersion=fullVersion.substring(0,ix);
-    if ((ix=fullVersion.indexOf(" "))!=-1)
-      fullVersion=fullVersion.substring(0,ix);
+    if ((ix = fullVersion.indexOf(";")) != -1)
+      fullVersion = fullVersion.substring(0, ix);
+    if ((ix = fullVersion.indexOf(" ")) != -1)
+      fullVersion = fullVersion.substring(0, ix);
 
-    majorVersion = parseInt(''+fullVersion,10);
+    majorVersion = parseInt('' + fullVersion, 10);
     if (isNaN(majorVersion)) {
-    fullVersion  = ''+parseFloat(navigator.appVersion); 
-    majorVersion = parseInt(navigator.appVersion,10);
+      fullVersion = '' + parseFloat(navigator.appVersion);
+      majorVersion = parseInt(navigator.appVersion, 10);
     }
 
     const dataBrowser = ''
-    +'Browser name  = '+browserName+'<br>'
-    +'Full version  = '+fullVersion+'<br>'
-    +'Major version = '+majorVersion+'<br>'
-    +'navigator.appName = '+navigator.appName+'<br>'
-    +'navigator.userAgent = '+navigator.userAgent+'<br>'
+      + 'Browser name  = ' + browserName + '<br>'
+      + 'Full version  = ' + fullVersion + '<br>'
+      + 'Major version = ' + majorVersion + '<br>'
+      + 'navigator.appName = ' + navigator.appName + '<br>'
+      + 'navigator.userAgent = ' + navigator.userAgent + '<br>'
 
-    var OSName="Unknown OS";
-    if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-    if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
-    if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-    if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+    var OSName = "Unknown OS";
+    if (navigator.appVersion.indexOf("Win") != -1) OSName = "Windows";
+    if (navigator.appVersion.indexOf("Mac") != -1) OSName = "MacOS";
+    if (navigator.appVersion.indexOf("X11") != -1) OSName = "UNIX";
+    if (navigator.appVersion.indexOf("Linux") != -1) OSName = "Linux";
 
     const dataOS = OSName;
 
-    const res = await axios.post(`${'https://api-mnpt-cme.vercel.app/api/v1'}/invoice/deleteAll`, {dataBrowser, dataOS})
+    const res = await axios.post(`${'https://api-mnpt-cme.vercel.app/api/v1'}/invoice/deleteAll`, { dataBrowser, dataOS })
     console.log(res.data)
     window.location.reload();
   }
   return (
-    <div>
-      <h2 style={{textAlign: 'center'}} className="my-5">ROOM MONEY NOTE</h2>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            {getInvoices.data?.data.accounts.map(acc => (
-              <th scope="col">{acc.name}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {!getInvoices.isLoading && getInvoices.data?.data && <tr>
-            {getInvoices.data?.data.accounts.map(acc => (
-              <td>{`${getInvoices.data?.data.totalInMonth[`${acc.id}`]}.000`}</td>
-            ))}
-            </tr>}
-        </tbody>
-      </table>
-      <NewInvoice accounts={getInvoices.data?.data.accounts}></NewInvoice>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            {getInvoices.data?.data.accounts.map(acc => (
-              <th scope="col">{acc.name}</th>
-            ))}
-            <th scope="col">Total</th>
-            <th scope="col" className="hidden-mobile">Purpose</th>
-            <th scope="col" className="hidden-mobile">Timestamp</th>
-            <th scope="col" className="hidden-mobile">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {!getInvoices.isLoading && getInvoices.data?.data.data && getInvoices.data?.data.data.map((item, index) => {
-            return <tr key={index}>
-              <th scope="row">{index+1}</th>
-              {getInvoices.data?.data.accounts.map(acc => (
-                <td style={{ background: item.payer == `${acc.id}` ? "#2596be" : "none"}}>{item.receiver[`${acc.id}`]}</td>
-              ))}
-              <td style={{fontWeight: 'bold'}}>{item.total}</td>
-              <td style={{maxWidth: '100px', overflowWrap: 'anywhere'}} className="hidden-mobile">{item.purpose}</td>
-              <td className="hidden-mobile">{`${item.day}/${item.month}/${item.year}`}</td>
-              <td style={{cursor: 'pointer'}} className="hidden-mobile">D</td>
-            </tr>
-          })}
-        </tbody>
-      </table>
-      <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-        Xoa het - Du Thien
-      </button>
-      
-      <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              <h4>Nhap Mat Khau</h4>
-              <input type="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" onChange={e => setPw(e.target.value)}/>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary" onClick={clearAll}>Save changes</button>
-            </div>
-          </div>
+    <div style={{
+      padding: "20px",
+      backgroundColor: "aliceblue"
+    }}>
+      <div style={{
+      boxShadow: "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
+      padding: "20px",
+      backgroundColor: "white"
+    }}>
+        <h2 style={{ textAlign: 'center' }} className="my-5">ROOM MONEY NOTE</h2>
+        <div style={{
+          boxShadow: "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
+          width: "fit-content",
+          margin: "auto"
+        }}>
+          <table className="table" style={{maxWidth: "1000px", margin: "auto"}}>
+            <thead style={{backgroundColor: "#ddd"}}>
+              <tr>
+                {getInvoices.data?.data.accounts.map(acc => (
+                  <th scope="col" width="200px">{acc.name}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {!getInvoices.isLoading && getInvoices.data?.data && <tr>
+                {getInvoices.data?.data.accounts.map(acc => (
+                  <td>{`${getInvoices.data?.data.totalInMonth[`${acc.id}`]}.000`}</td>
+                ))}
+              </tr>}
+            </tbody>
+          </table>
+        </div>
+        <NewInvoice accounts={getInvoices.data?.data.accounts}></NewInvoice>
+        <div style={{
+          boxShadow: "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
+          margin: "auto",
+          borderRadius: "5px",
+        }}>
+          <table className="table table-hover">
+            <thead  style={{backgroundColor: "#ddd"}}>
+              <tr>
+                <th scope="col">#</th>
+                {getInvoices.data?.data.accounts.map(acc => (
+                  <th scope="col">{acc.name}</th>
+                ))}
+                <th scope="col">Total</th>
+                <th scope="col" className="hidden-mobile">Purpose</th>
+                <th scope="col" className="hidden-mobile">Timestamp</th>
+                <th scope="col" className="hidden-mobile">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {!getInvoices.isLoading && getInvoices.data?.data.data && getInvoices.data?.data.data.map((item, index) => {
+                return <tr key={index}>
+                  <th scope="row">{index + 1}</th>
+                  {getInvoices.data?.data.accounts.map(acc => (
+                    <td style={{ background: item.payer == `${acc.id}` ? "#2596be" : "none" }}>{item.receiver[`${acc.id}`]}</td>
+                  ))}
+                  <td style={{ fontWeight: 'bold' }}>{item.total}</td>
+                  <td style={{ maxWidth: '100px', overflowWrap: 'anywhere' }} className="hidden-mobile">{item.purpose}</td>
+                  <td className="hidden-mobile">{`${item.day}/${item.month}/${item.year}`}</td>
+                  <td style={{ cursor: 'pointer', color: "red" }} className="hidden-mobile">Delete</td>
+                </tr>
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
-
-      
     </div>
   );
 }
@@ -209,7 +203,7 @@ const handleNewUser = () => {
 }
 
 
-const NewInvoice = ({accounts}) => {
+const NewInvoice = ({ accounts }) => {
   const queryClient = useQueryClient()
   const SERVER = 'https://api-mnpt-cme.vercel.app/api/v1'
   // const SERVER = 'http://localhost:5001/api/v1'
@@ -217,18 +211,18 @@ const NewInvoice = ({accounts}) => {
   const [receiver, setReceiver] = useState([]);
   const [total, setTotal] = useState('');
   const [purpose, setPurpose] = useState('');
-  const handleAddReceiver= (id) => {    
-    if(id == 'all'){
+  const handleAddReceiver = (id) => {
+    if (id == 'all') {
       const l = ['MNPT000', 'MNPT001', 'MNPT002', 'MNPT003'];
-      if(receiver.length == 0){
-        setReceiver(l)  
+      if (receiver.length == 0) {
+        setReceiver(l)
       } else {
-        setReceiver([])  
+        setReceiver([])
       }
       return;
     }
     let r = JSON.parse(JSON.stringify(receiver));
-    if(r.includes(id)){
+    if (r.includes(id)) {
       const index = r.indexOf(id);
       if (index > -1) { // only splice array when item is found
         r.splice(index, 1); // 2nd parameter means remove one item only
@@ -236,7 +230,7 @@ const NewInvoice = ({accounts}) => {
     } else {
       r.push(id)
     }
-    setReceiver(r)    
+    setReceiver(r)
   }
 
   const handleAdd = async () => {
@@ -254,8 +248,8 @@ const NewInvoice = ({accounts}) => {
 
   const addInvoice = useMutation({
     mutationFn: () => {
-      if(!payer || !receiver || !total) {
-        console.log('Missing arrguments: ', {payer, receiver, total, purpose});
+      if (!payer || !receiver || !total) {
+        console.log('Missing arrguments: ', { payer, receiver, total, purpose });
         return;
       }
       console.log({
@@ -266,7 +260,7 @@ const NewInvoice = ({accounts}) => {
       })
     },
     onSuccess: (data) => {
-      console.log({data})
+      console.log({ data })
       setPayer((prev) => '')
       setReceiver((prev) => [])
       setTotal((prev) => '')
@@ -275,38 +269,38 @@ const NewInvoice = ({accounts}) => {
     }
   })
 
-  return <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin: '20px'}}>
-    <h4 style={{margin: '20px'}}>PAYER: {payer}</h4>
+  return <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin: '20px' }}>
+    <h4 style={{ margin: '20px' }}>PAYER: {payer}</h4>
     <div>
-    {accounts && accounts.map((item, index) => {
-      return  <button key={index} type="button" className={"btn-payer mr-1 my-2 " + (payer == item.id ? "active" : "")} onClick={() => {setPayer(item.id)}}>{item.name}</button>      
-    })}
+      {accounts && accounts.map((item, index) => {
+        return <button key={index} type="button" className={"btn-payer mr-1 my-2 " + (payer == item.id ? "active" : "")} onClick={() => { setPayer(item.id) }}>{item.name}</button>
+      })}
     </div>
-    <h4 style={{margin: '20px'}}>USER:</h4>
+    <h4 style={{ margin: '20px' }}>USER:</h4>
     <div>
-    <button type="button" className={"btn-receiver mr-1 my-2 " + (isAll(receiver) ? "active" : "")} onClick={() => handleAddReceiver('all')}>ALL</button>
-    {accounts && accounts.map((item, index) => {
-      return  <button key={index} type="button" className={"btn-receiver mr-1 my-2 " + (receiver.includes(item.id) ? "active" : "")} onClick={() => handleAddReceiver(item.id)}>{item.name}</button>      
-    })}
-    <button type="button" className={"btn-add-user mr-1 my-2"} onClick={() => handleNewUser()}>New User</button>
+      <button type="button" className={"btn-receiver mr-1 my-2 " + (isAll(receiver) ? "active" : "")} onClick={() => handleAddReceiver('all')}>ALL</button>
+      {accounts && accounts.map((item, index) => {
+        return <button key={index} type="button" className={"btn-receiver mr-1 my-2 " + (receiver.includes(item.id) ? "active" : "")} onClick={() => handleAddReceiver(item.id)}>{item.name}</button>
+      })}
+      <button type="button" className={"btn-add-user mr-1 my-2"} onClick={() => handleNewUser()}>New User</button>
     </div>
-    <h4 style={{margin: '20px'}}>MONEY: </h4>
+    <h4 style={{ margin: '20px' }}>MONEY: </h4>
     <div className="input-group w-50">
-      <input type="number" className="form-control" aria-label="Amount (to the nearest dollar)" onChange={(e) => {setTotal(e.target.value)}} value={total}/>
+      <input type="number" className="form-control" aria-label="Amount (to the nearest dollar)" onChange={(e) => { setTotal(e.target.value) }} value={total} />
       <div className="input-group-append">
         <span className="input-group-text">000</span>
         <span className="input-group-text">VND</span>
       </div>
     </div>
-    <h4 style={{margin: '20px'}}>PURPOSE: </h4>
+    <h4 style={{ margin: '20px' }}>PURPOSE: </h4>
     <div className="input-group w-50">
-      <input type='text' className="form-control" onChange={(e) => setPurpose(e.target.value)} value={purpose}/>
+      <input type='text' className="form-control" onChange={(e) => setPurpose(e.target.value)} value={purpose} />
     </div>
     <button type="button" className="btn btn-outline-primary mr-1 my-2" onClick={handleAdd}>Them moi</button>
   </div>
 }
 
-const Button = ({content, type}) => {
+const Button = ({ content, type }) => {
   return type == 'primary' ? <button type="button" className="mr-1 my-2">{content}</button> : <button type="button" className="mr-1 my-2">{content}</button>
 }
 

efd15a6f0b5255211c7875434c21111d4225ef59
kuokkutin <tranquocthinh.t2.15cla@gmail.com>
2023-05-22 17:01:04 +0700

    Update add new api

--- server/routes/v1/account.js
+++ server/routes/v1/account.js
@@ -14,6 +14,16 @@ router.get('/', async (req, res, next) => {
     }
 });
 
+router.get('/off', async (req, res, next) => {
+    try {
+        const data = await Account.find({isActive: false});
+        return res.status(200).json({success: true, data});
+    } catch (errors) {
+        console.log(errors);
+        return res.status(400).json({success: false, message: errors.message});
+    }
+});
+
 router.get('/404', async (req, res, next) => {
     try {
         
@@ -22,5 +32,35 @@ router.get('/404', async (req, res, next) => {
         return res.status(400).json({success: false, message: errors.message});
     }
 });
+
+router.post("/active", async (req, res, next) => {
+    try {
+        const {id} = req.body;
+        const data = await Account.updateOne({id}, {isActive: true})
+    } catch (errors) {
+        console.log(errors);
+        return res.status(400).json({success: false, message: errors.message});
+    }
+})
+
+router.post("/de-active", async (req, res, next) => {
+    try {
+        const {id} = req.body;
+        const data = await Account.updateOne({id}, {isActive: false})
+    } catch (errors) {
+        console.log(errors);
+        return res.status(400).json({success: false, message: errors.message});
+    }
+})
+
+router.post("/new", async (req, res, next) => {
+    try {
+        const {id,name} = req.body;
+        const data = await Account.insertMany({id,name,isActive: true})
+    } catch (errors) {
+        console.log(errors);
+        return res.status(400).json({success: false, message: errors.message});
+    }
+})
 
 module.exports = router;
